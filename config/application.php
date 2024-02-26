f89029b1006c1556aa068125e65e938e2f02103a
Hieu Tran <tr.kimhieu@gmail.com>
2024-02-27 05:58:37 +0700

    Add ALLOW_UNFILTERED_UPLOADS

--- config/application.php
+++ config/application.php
@@ -112,6 +112,8 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
 Config::define('AUTOMATIC_UPDATER_DISABLED', true);
 Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 
+Config::define('ALLOW_UNFILTERED_UPLOADS', true);
+
 // Disable the plugin and theme file editor in the admin
 Config::define('DISALLOW_FILE_EDIT', true);
 
